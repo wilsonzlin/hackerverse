c614ac5ba406b5bc8988c4d8ffc955b9bf98d898
Wilson Lin <code@wilsonl.in>
2024-04-12 16:46:53 +1000

    Create nodejs-base image

--- wrench/queue.ts
+++ wrench/queue.ts
@@ -1,10 +1,10 @@
 import { Command } from "sacli";
-import { QUEUE_HN_CRAWL } from "../common/res";
+import { QUEUE_CRAWL } from "../common/res";
 
 const cli = Command.new("queue");
 
 cli.subcommand("metrics").action(async () => {
-  console.table(await QUEUE_HN_CRAWL.metrics());
+  console.table(await QUEUE_CRAWL.metrics());
 });
 
 cli.eval(process.argv.slice(2));
