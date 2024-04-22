7af3e2b1eee5d8ac595f1dc1fa75431680ed57c1
Wilson Lin <code@wilsonl.in>
2024-04-22 07:39:05 +0000

    Split into tiles

--- build-map/main.py
+++ build-map/main.py
@@ -3,7 +3,9 @@ from common.data import load_table
 from common.emb_data import load_emb_table_ids
 from multiprocessing import Pool
 from pandas import DataFrame
+import json
 import numpy as np
+import os
 import pandas as pd
 import rtree
 import struct
@@ -17,8 +19,8 @@ INCLUDE_COMMENTS = False
 # ...
 # At the maximum zoom level, we show everything, regardless of distance.
 ZOOM_LEVELS = 8
-BASE_DISTANCE = 0.25
-BASE_AXIS_TILES = 8
+BASE_DISTANCE = 0.2
+BASE_AXIS_TILES = 1
 
 
 def build_map_at_zoom_level(zoom_level: int):
@@ -49,6 +51,19 @@ def build_map_at_zoom_level(zoom_level: int):
 
     if zoom_level == ZOOM_LEVELS - 1:
         lg("No filtering needed")
+        x_min, x_max = df["x"].min(), df["x"].max()
+        y_min, y_max = df["y"].min(), df["y"].max()
+        with open("/hndr-data/map.json", "w") as f:
+            json.dump(
+                {
+                    "x_min": x_min.item(),
+                    "x_max": x_max.item(),
+                    "y_min": y_min.item(),
+                    "y_max": y_max.item(),
+                    "count": count,
+                },
+                f,
+            )
     else:
         lg("Sorting data")
         df = df.sort_values("score", ascending=False)
@@ -72,6 +87,42 @@ def build_map_at_zoom_level(zoom_level: int):
         count = len(df)
         assert count == len(filtered)
         lg("Filtered to", count, "points")
+
+    axis_tile_count = BASE_AXIS_TILES * (2**zoom_level)
+    lg("Tiling to", axis_tile_count * axis_tile_count, "tiles")
+    x_min, x_max = df["x"].min(), df["x"].max()
+    y_min, y_max = df["y"].min(), df["y"].max()
+    x_tile_width = (x_max - x_min) / axis_tile_count
+    y_tile_width = (y_max - y_min) / axis_tile_count
+    # The point that lies at x_max or y_max needs to be clipped to the last tile.
+    df["tile_x"] = (
+        ((df["x"] - x_min) // x_tile_width)
+        .clip(upper=axis_tile_count - 1)
+        .astype("uint32")
+    )
+    df["tile_y"] = (
+        ((df["y"] - y_min) // y_tile_width)
+        .clip(upper=axis_tile_count - 1)
+        .astype("uint32")
+    )
+
+    for _, tile_data in df.groupby(["tile_x", "tile_y"]):
+        tile_x = tile_data["tile_x"].iloc[0]
+        tile_y = tile_data["tile_y"].iloc[0]
+        vec_id = tile_data["id"].to_numpy()
+        vec_x = tile_data["x"].to_numpy()
+        vec_y = tile_data["y"].to_numpy()
+        vec_score = tile_data["score"].to_numpy()
+        assert vec_id.shape == vec_x.shape == vec_y.shape == vec_score.shape
+        tile_count = vec_id.shape[0]
+        d = f"/hndr-data/map/z{zoom_level}"
+        os.makedirs(d, exist_ok=True)
+        with open(f"{d}/{tile_x}-{tile_y}.bin", "wb") as f:
+            f.write(struct.pack("<I", tile_count))
+            f.write(vec_id.tobytes())
+            f.write(vec_x.tobytes())
+            f.write(vec_y.tobytes())
+            f.write(vec_score.tobytes())
 
     vec_id = df["id"].to_numpy()
     vec_x = df["x"].to_numpy()
