276f454463d3e848ec69faeec9191d8a54d74d34
Wilson Lin <code@wilsonl.in>
2024-05-12 15:08:00 +1000

    Improve post links

--- app/component/Post.tsx
+++ app/component/Post.tsx
@@ -22,12 +22,7 @@ export const Post = ({
   const snippet = (urlMeta?.description || urlMeta?.snippet)?.trim();
   const imgUrl = urlMeta?.image_url;
   return (
-    <a
-      className="Post"
-      href={`${proto}//${url}`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
+    <div className="Post">
       <div className="text">
         <div className="header">
           <ImageOnLoad
@@ -41,16 +36,37 @@ export const Post = ({
               )}
               <div className="site">{domain}</div>
             </div>
-            <h2>{p.title}</h2>
+            <h2>
+              <a
+                href={`${proto}//${url}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {p.title}
+              </a>
+            </h2>
             <div className="sub">
-              {p.score} points by {p.author}{" "}
-              {DateTime.fromJSDate(p.ts).toRelative()}
+              {p.score} points by{" "}
+              <a
+                href={`https://news.ycombinator.com/user?id=${p.author}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {p.author}
+              </a>{" "}
+              <a
+                href={`https://news.ycombinator.com/item?id=${id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {DateTime.fromJSDate(p.ts).toRelative()}
+              </a>
             </div>
           </div>
         </div>
         {snippet && <p className="snippet">{snippet}</p>}
       </div>
       {imgUrl && !hideImage && <ImageOnLoad className="image" src={imgUrl} />}
-    </a>
+    </div>
   );
 };
